4c983bf1ef6a079c32097056ffbf4186801b3abf
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-11-29 19:17:12 +0530

    t

--- prasanna-11/src/App.js
+++ prasanna-11/src/App.js
@@ -1,8 +1,8 @@
-import Bind from './Bind'
+import Date from './Date'
 let App=()=>{
 return <div>
     <h1 className="navbar-dark bg-success">Welcome to ReactJs</h1>
-    <Bind/>
+    <Date/>
 </div>
 }
 export default App
